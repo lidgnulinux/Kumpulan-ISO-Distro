e6509dd11974f403f664542b1cc7031178ae3dbe
Λdiƛin <muhamadaviv14@gmail.com>
2023-03-06 14:32:17 +0700

    Update check-link.sh
    fixed script

--- check-link.sh
+++ check-link.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-link=$(grep https README.md | cut -d ']' -f 2 | tr -d "()")
+set -eo pipefail
 
-for i in $link
-do
-    status_code=$(curl -Is $i | head -n 1 | cut -d ' ' -f 2)
-    if [ $status_code -ne 200 ]; then
-        echo "Error accessing $i. Status code: $status_code"
+link=$(grep -oP '(?<=\]\().*?(?=\))' README.md)
+
+while IFS= read -r i; do
+    status_code=$(curl --head --silent --location --output /dev/null --write-out "%{http_code}" "$i")
+    if [ "$status_code" -ne 200 ]; then
+        echo "Error accessing $i Status code: $status_code"
         exit 1
     fi
-done
+done <<< "$link"
 
 echo "All links are accessible"
 exit 0
