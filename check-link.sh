f86122b0af035abb8880ef35330c0b8965343295
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-06 14:03:55 +0700

    fix if link response except 200 will return 1
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- check-link.sh
+++ check-link.sh
@@ -4,11 +4,12 @@ link=$(grep https README.md | cut -d ']' -f 2 | tr -d "()")
 
 for i in $link
 do
-	if ! wget --spider $i; then
-		echo "Failed to access $i"
-		break
-	fi
+    status_code=$(curl -Is $i | head -n 1 | cut -d ' ' -f 2)
+    if [ $status_code -ne 200 ]; then
+        echo "Error accessing $i. Status code: $status_code"
+        exit 1
+    fi
 done
 
-exit
-
+echo "All links are accessible"
+exit 0
