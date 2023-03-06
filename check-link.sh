1e8906a25492c5419684187892e1a8ffebd5c7e5
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-06 13:54:36 +0700

    if link error exit loop
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- check-link.sh
+++ check-link.sh
@@ -4,5 +4,11 @@ link=$(grep https README.md | cut -d ']' -f 2 | tr -d "()")
 
 for i in $link
 do
-	wget --spider $i
+	if ! wget --spider $i; then
+		echo "Failed to access $i"
+		break
+	fi
 done
+
+exit
+
